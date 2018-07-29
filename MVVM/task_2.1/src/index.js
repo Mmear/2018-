e9f5836c8d2efec97b4abecf78b8acc761d3f3ff
Mirr <mmearwzf@qq.com>
2018-07-29 17:14:22 +0800

    finished task2.1

---
+++ MVVM/task_2.1/src/index.js
@@ -0,0 +1,5 @@
+import {router} from 'san-router';
+import app from './app.san';
+
+router.add({rule: '/', Component: app, target: '#app'});
+router.start();
---
+++ MVVM/task_2.1/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'MVVM Task_1.2'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.san$/,
+                use: 'san-loader'
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
